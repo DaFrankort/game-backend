632176a8f9b7c1aaf2bf0644370162e8c3733dc0
ImEldie <daniel.frankort@telenet.be>
2026-03-15 19:45:56 +0100

    Add HostUserName to LobbySummaryDto

--- server/Source/DTO/Lobby.cs
+++ server/Source/DTO/Lobby.cs
@@ -22,6 +22,7 @@ namespace Server.DTO
     {
         public string Id { get; set; } = lobby.Id;
         public string Name { get; set; } = lobby.Name;
+        public string HostUserName { get; set; } = lobby.Host.Name;
         public int MemberCount { get; set; } = lobby.Members.Count;
         public int MaxMembers { get; set; } = lobby.MaxMembers;
     }
