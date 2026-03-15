b4ead0d88d983e9160102e72bc5b9f4baf1469a0
ImEldie <daniel.frankort@telenet.be>
2026-03-15 17:35:06 +0100

    Add validators

--- server/Source/DTO/Lobby.cs
+++ server/Source/DTO/Lobby.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
 using Server.Models;
 
 namespace Server.DTO
 {
     public class CreateLobbyRequestDto
     {
+        [Required(ErrorMessage = "Lobby name is required.")]
+        [StringLength(
+            20,
+            MinimumLength = 3,
+            ErrorMessage = "Lobby name must be between 3 and 20 characters."
+        )]
+        [RegularExpression(
+            @"^[a-zA-Z0-9_-]+$",
+            ErrorMessage = "User name can only contain letters, numbers, underscores, and hyphens."
+        )]
         public string Name { get; set; } = "Lobby";
     }
 
--- server/Source/DTO/User.cs
+++ server/Source/DTO/User.cs
@@ -1,7 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using Server.Models;
 
 public class CreateUserRequestDto
 {
+    [Required(ErrorMessage = "User name is required.")]
+    [StringLength(
+        20,
+        MinimumLength = 3,
+        ErrorMessage = "User name must be between 3 and 20 characters."
+    )]
+    [RegularExpression(
+        @"^[a-zA-Z0-9_-]+$",
+        ErrorMessage = "User name can only contain letters, numbers, underscores, and hyphens."
+    )]
     public string Name { get; set; } = "User";
 }
 
